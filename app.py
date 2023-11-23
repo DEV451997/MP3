e2b71fa72164490b4b6617f7a345ef93108a1e2a
Arron Evans <aredev45@gmail.com>
2023-11-23 13:38:39 +0000

    Created the template for add_car.

--- app.py
+++ app.py
@@ -93,6 +93,11 @@ def logout():
     return redirect(url_for("login"))
 
 
+@app.route("/add_car")
+def add_car():
+    return render_template("add_car.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
