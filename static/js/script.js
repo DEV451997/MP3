3c303ad52293846dbe14ff83ece50132858ec47e
Arron Evans <aredev45@gmail.com>
2023-11-28 19:20:02 +0000

    Updated password function to jquery.

--- static/js/script.js
+++ static/js/script.js
@@ -3,19 +3,19 @@ $(document).ready(function () {
     $('select').formSelect();
 });
 
-document.addEventListener('DOMContentLoaded', function() {
-    const passwordInput = document.getElementById('password');
-    const togglePassword = document.querySelector('.toggle-password');
+$(document).ready(function() {
+    const passwordInput = $('#password');
+    const togglePassword = $('.toggle-password');
 
-    togglePassword.addEventListener('click', function() {
-        if (passwordInput.type === 'password') {
-            passwordInput.type = 'text';
-            togglePassword.classList.remove('fa-eye');
-            togglePassword.classList.add('fa-eye-slash');
+    togglePassword.click(function() {
+        if (passwordInput.attr('type') === 'password') {
+            passwordInput.attr('type', 'text');
+            togglePassword.removeClass('fa-eye');
+            togglePassword.addClass('fa-eye-slash');
         } else {
-            passwordInput.type = 'password';
-            togglePassword.classList.remove('fa-eye-slash');
-            togglePassword.classList.add('fa-eye');
+            passwordInput.attr('type', 'password');
+            togglePassword.removeClass('fa-eye-slash');
+            togglePassword.addClass('fa-eye');
         }
     });
 });
