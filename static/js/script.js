abafaf9c937146d5f5d34e234dba2185d7414221
Arron Evans <aredev45@gmail.com>
2023-11-28 13:57:40 +0000

    Created function to allow user to view or hide password.

--- static/js/script.js
+++ static/js/script.js
@@ -2,3 +2,20 @@ $(document).ready(function () {
     $(".sidenav").sidenav({edge: "right"});
     $('select').formSelect();
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const passwordInput = document.getElementById('password');
+    const togglePassword = document.querySelector('.toggle-password');
+
+    togglePassword.addEventListener('click', function() {
+        if (passwordInput.type === 'password') {
+            passwordInput.type = 'text';
+            togglePassword.classList.remove('fa-eye');
+            togglePassword.classList.add('fa-eye-slash');
+        } else {
+            passwordInput.type = 'password';
+            togglePassword.classList.remove('fa-eye-slash');
+            togglePassword.classList.add('fa-eye');
+        }
+    });
+});
