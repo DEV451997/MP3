8d931e796a9d962bc878da207330b361a34c770e
Arron Evans <aredev45@gmail.com>
2023-11-28 18:46:56 +0000

    Made Python code pep8 complient.

--- app.py
+++ app.py
@@ -24,10 +24,11 @@ def get_stats():
     stats = list(mongo.db.stats.find())
     return render_template("stats.html", stats=stats)
 
+
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
-        
+
         existing_user = mongo.db.users.find_one(
             {"username": request.form.get("username").lower()})
 
@@ -45,37 +46,39 @@ def register():
         flash("Registration Successful!")
     return render_template("register.html")
 
+
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
-        
+
         existing_user = mongo.db.users.find_one(
             {"username": request.form.get("username").lower()})
 
         if existing_user:
-            
+
             if check_password_hash(
                     existing_user["password"], request.form.get("password")):
-                        session["user"] = request.form.get("username").lower()
-                        flash("Welcome, {}".format(
+                session["user"] = request.form.get("username").lower()
+                flash("Welcome, {}".format(
                             request.form.get("username")))
-                        return redirect(url_for(
+                return redirect(url_for(
                             "profile", username=session["user"]))
             else:
-                
+
                 flash("Incorrect Username and/or Password")
                 return redirect(url_for("login"))
 
         else:
-            
+
             flash("Incorrect Username and/or Password")
             return redirect(url_for("login"))
 
     return render_template("login.html")
 
+
 @app.route("/profile/<username>", methods=["GET", "POST"])
 def profile(username):
-    
+
     username = mongo.db.users.find_one(
         {"username": session["user"]})["username"]
 
@@ -87,11 +90,10 @@ def profile(username):
 
 @app.route("/logout")
 def logout():
-    
+
     flash("You have been logged out")
     session.pop("user")
     return redirect(url_for("login"))
-
 
 
 @app.route("/add_car", methods=["GET", "POST"])
@@ -110,7 +112,6 @@ def add_car():
         flash("Car Successfully Added")
         return redirect(url_for("get_stats"))
 
-
     categories = mongo.db.categories.find().sort("category_name", 1)
     return render_template("add_car.html", categories=categories)
 
@@ -126,14 +127,15 @@ def edit_car(stats_id):
             "horsepower": request.form.get("horsepower"),
             "image_url": request.form.get("image_url"),
         }
-        mongo.db.stats.update_one({"_id": ObjectId(stats_id)}, {"$set": submit})
+        mongo.db.stats.update_one({"_id": ObjectId(stats_id)},
+                                  {"$set": submit})
         flash("Car Successfully Updated")
-        
-    
+
     stats_id_obj = ObjectId(stats_id)
     stat = mongo.db.stats.find_one({"_id": stats_id_obj})
     categories = mongo.db.categories.find().sort("category_name", 1)
     return render_template("edit_car.html", stat=stat, categories=categories)
+
 
 @app.route("/delete_car/<stats_id>")
 def delete_car(stats_id):
