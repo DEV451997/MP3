38167b331aca5f5d72fd63758e4aa31617e32f5f
Arron Evans <aredev45@gmail.com>
2023-11-30 15:10:38 +0000

    Debug set to false prior to submission

--- app.py
+++ app.py
@@ -146,4 +146,4 @@ def delete_car(stats_id):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
