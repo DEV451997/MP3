307d36d23c63d9c1149e6bee120caa4e2fafab35
Arron Evans <aredev45@gmail.com>
2023-11-30 15:53:54 +0000

    Added comments to all HTML, CSS and Javascript files

--- static/js/script.js
+++ static/js/script.js
@@ -1,10 +1,11 @@
+// Materialize Jquery to initialize functions
 $(document).ready(function () {
     $(".sidenav").sidenav({
         edge: "right"
     });
     $('select').formSelect();
 });
-
+// Password function
 $(document).ready(function () {
     const passwordInput = $('#password');
     const togglePassword = $('.toggle-password');
@@ -21,7 +22,7 @@ $(document).ready(function () {
         }
     });
 });
-
+// Initialize the modal
 $(document).ready(function () {
     $('.modal').modal();
 });
